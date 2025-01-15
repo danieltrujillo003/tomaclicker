01fc3118b071320e9ef3c3d1eadf42510a8a39db
daniel.trujillo <daniel.trujillo@geniussports.com>
2025-01-15 17:02:52 -0500

    Added tomato song

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const mainTomato = document.getElementById('mainTomato')
-const counterDisplay = document.getElementById('counterDisplay')
+const mainTomato = document.getElementById('main-tomato')
+const counterDisplay = document.getElementById('counter-display')
+const tomatoSong = document.getElementById('tomato-song')
 
 let counter = 0
 
 counterDisplay.innerText = Math.floor(counter)
 
 mainTomato.addEventListener('click', function (e) {
+  counter++
+
+  if (counter === 95) {
+    tomatoSong.volume = 0.1;
+    tomatoSong.play();
+  }
+
   if (counter > 100) counter *= 1.1
   if (counter > 300) counter *= 1.2
   if (counter > 10000) counter *= 1.3
   if (counter > 1000000) counter *= 2.0
-  else counter++
+
   counterDisplay.innerText = Math.floor(counter)
   createDrop()
   removeDrop()
