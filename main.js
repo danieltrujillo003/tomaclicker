e7722465a44b85ebac48e3a810c18e6b086377e3
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2025-01-15 18:12:11 -0500

    correction to sound playing at 0, now starts at 95 clicks

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ frase.style.display = 'none'
 // set the splash sound
 const tomatoSplash = new Audio('assets/tomatoe.mp3')
 
-let counter = 94
+let counter = 0
 //let volumeSong = 0.1
 counterDisplay.innerText = Math.floor(counter)
 
@@ -22,22 +22,12 @@ mainTomato.addEventListener('click', function (e) {
   if (counter > 95){
     tomatoSong.volume += 0.005
   }
-  // if (counter < 1000 && counter > 95) tomatoSong.volume = 0.3
-  // if (counter < 1000000 && counter > 1000) tomatoSong.volume = 0.5
-  // if (counter < 100000000 && counter > 1000000) tomatoSong.volume = 0.7
-  // if (counter < 100000000000 && counter > 100000000) tomatoSong.volume = 0.8
-  tomatoSong.play();
+  
   //if ( counter === 100 ) playRomanticSong()
   if (counter > 100) counter *= 1.1
   if (counter > 300) counter *= 1.2
   if (counter > 10000) counter *= 1.3
   if (counter > 1000000) counter *= 2.0
-  // Lines to control the splash sound
-  tomatoSplash.play()
-  setTimeout(() => {
-    tomatoSplash.pause()
-    tomatoSplash.currentTime = 0
-  }, 300)
 
   counterDisplay.innerText = Math.floor(counter)
   createDrop()
